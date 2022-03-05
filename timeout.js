4ad88f19408d774578203db5562722e45fe4c2d8
MdRakibulIslam404 <mdrakibulislam752@gmail.com>
2022-03-06 01:09:30 +0600

    setTimeout setInterval synchronous-asynchronous browser-console-invs-code-and-debug https-how-internet-works-dom-tree-render-tree-v8-engine-etc

---
+++ timeout.js
@@ -0,0 +1,15 @@
+function doSomething() {
+    console.log("I am coding JavaScript");
+};
+
+console.log("First: ami sobar age");
+console.log("Second: I am the second person");
+// setTimeout(doSomething, 5000);
+setTimeout(function () {
+    console.log("I'm using VS code");
+}, 5000);
+setTimeout(() => {
+    console.log("Exploring MDN articles");
+}, 4000);
+console.log("Third: ami 3 number baccha");
+console.log("Fourth: ami ek hali purno korchi");
